630afb05ea946f7f277c75cc32a7faf28c9390c7
Lyesome <aleciapiva@gmail.com>
2018-04-08 01:10:09 -0600

    Added steering

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMode.java
@@ -85,15 +85,31 @@ public class DriverMode extends LinearOpMode {
             telemetry.update();
 
             //Drive System
-            motorDriveL.setPower(gamepad1.left_stick_y);
-            motorDriveR.setPower(gamepad1.left_stick_y);
-            motorDriveL.setPower(gamepad1.right_stick_x);
-            motorDriveR.setPower(-gamepad1.right_stick_x);
-            if (gamepad1.x){
-                servoFlap.setPosition(FLAP_OPEN);
-                sleep(FLAP_WAIT);
-                servoFlap.setPosition(FLAP_CLOSE);
+            if (gamepad1.left_stick_y >= 0) {
+                if (gamepad1.right_stick_x >= 0){
+                    motorDriveL.setPower(gamepad1.left_stick_y);
+                    motorDriveR.setPower(gamepad1.left_stick_y - gamepad1.right_stick_x);
+                }
+                if (gamepad1.right_stick_x < 0) {
+                    motorDriveL.setPower(gamepad1.left_stick_y + gamepad1.right_stick_x);
+                    motorDriveR.setPower(gamepad1.left_stick_y);
+                }
             }
+            if (gamepad1.left_stick_y < 0) {
+                if (gamepad1.left_stick_x >= 0){
+                    motorDriveL.setPower(gamepad1.left_stick_y);
+                    motorDriveR.setPower(gamepad1.left_stick_y + gamepad1.right_stick_x);
+                }
+                if (gamepad1.right_stick_x < 0) {
+                    motorDriveL.setPower(gamepad1.left_stick_y);
+                    motorDriveR.setPower(gamepad1.left_stick_y - gamepad1.right_stick_x);
+                }
+            }
+//            if (gamepad1.x){
+//                servoFlap.setPosition(FLAP_OPEN);
+//                sleep(FLAP_WAIT);
+//                servoFlap.setPosition(FLAP_CLOSE);
+//            }
             if (gamepad1.right_trigger < 0.5 ){
                 motorShooterR.setPower(SHOOTER_POWER);
                 motorShooterL.setPower(SHOOTER_POWER);
